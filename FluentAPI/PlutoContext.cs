fb0b69b316aa8a92d9831172fd7b027189702dde
Peter Menner <peter.menner@uky.edu>
2018-02-08 16:12:52 -0500

    Initial Commit

---
+++ FluentAPI/PlutoContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentAPI
+{
+	public class PlutoContext : DbContext
+	{
+		public PlutoContext() : base("name=PlutoConnection")
+		{
+
+		}
+
+		public virtual DbSet<Author> Authors { get; set; }
+		public virtual DbSet<Course> Courses { get; set; }
+		public virtual DbSet<Tag> Tags { get; set; }
+	}
+}
